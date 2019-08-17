5cfe98d9fb496a96a08c240ae3c3487e53180554
Alex Edward Ball <ballalexedward@gmail.com>
2019-08-17 13:32:51 -0700

    working app syncing with db

--- models/requests.js
+++ models/requests.js
@@ -1,32 +1,32 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Requests = sequelize.define('Requests', {
-    request_number: DataTypes.STRING,
-    work_order_number: DataTypes.STRING,
-    request_date: DataTypes.STRING,
-    date_approved: DataTypes.STRING,
-    date_due: DataTypes.STRING,
-    status: DataTypes.STRING,
-    date_completed: DataTypes.STRING,
-    assigned_to: DataTypes.STRING,
-    allowed_to_enter: DataTypes.BOOLEAN,
-    item1: Datatypes.STRING,
-    item1_category: Datatypes.STRING,
-    item2: Datatypes.STRING,
-    item2_category: Datatypes.STRING,
-    item3: Datatypes.STRING,
-    item3_category: Datatypes.STRING,
-    item4: Datatypes.STRING,
-    item4_category: Datatypes.STRING,
-    item5: Datatypes.STRING,
-    item5: Datatypes.STRING,
-    comments_feedback: DataTypes.STRING,
-    overall_job_rating_tenant: DataTypes.STRING,
-    cleanliness_rating_supervisor: DataTypes.STRING,
-    overall_job_rating_supervisor: DataTypes.STRING
-  }, {});
-  Requests.associate = function(models) {
-    // associations can be defined here
-  };
-  return Requests;
+    const Requests = sequelize.define('Requests', {
+        request_number: DataTypes.STRING,
+        work_order_number: DataTypes.STRING,
+        request_date: DataTypes.STRING,
+        date_approved: DataTypes.STRING,
+        date_due: DataTypes.STRING,
+        status: DataTypes.STRING,
+        date_completed: DataTypes.STRING,
+        assigned_to: DataTypes.STRING,
+        allowed_to_enter: DataTypes.BOOLEAN,
+        item1: DataTypes.STRING,
+        item1_category: DataTypes.STRING,
+        item2: DataTypes.STRING,
+        item2_category: DataTypes.STRING,
+        item3: DataTypes.STRING,
+        item3_category: DataTypes.STRING,
+        item4: DataTypes.STRING,
+        item4_category: DataTypes.STRING,
+        item5: DataTypes.STRING,
+        item5: DataTypes.STRING,
+        comments_feedback: DataTypes.STRING,
+        overall_job_rating_tenant: DataTypes.STRING,
+        cleanliness_rating_supervisor: DataTypes.STRING,
+        overall_job_rating_supervisor: DataTypes.STRING
+    }, {});
+    Requests.associate = function (models) {
+        // associations can be defined here
+    };
+    return Requests;
 };
--- server.js
+++ server.js
@@ -1,46 +1,29 @@
-var express = require('express');  
-var Sequelize = require('sequelize');  
-var config = require('./config/config.json');  
-var bodyParser = require('body-parser');  
-  
-var password = config.password ? config.password : null;  
-  
-var sequelize = new Sequelize(  
-    config.database,  
-    config.user,  
-    config.password, {  
-        logging: console.log,  
-        define: {  
-            timestamps: false  
-        }  
-    }  
-);  
-  
-  
-var app = express();  
-var port = 3000;  
-var router = express.Router(); // will help in adding routes  
-app.use(bodyParser.urlencoded());  
-app.use(bodyParser.json()); 
+//Dependencies
+//=====================================
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-var bookModel = sequelize.define('books', { //define your model  
-    "bookName": Sequelize.INTEGER,  
-    "bookPrice": Sequelize.INTEGER  
-    });  
+const db = require("./models");
 
-    app.use('/api', router); //this will add routing to your api.  
-    //your api endpoint  localhost:3000/api/books  
-      
-    router.get('/books', function(req, res) {  
-        bookModel.findAll().  
-        then(function(books) {  
-            res.status(200).json(books);  
-        }, function(error) {  
-           res.status(500).send(error);  
-        });  
-    });  
-      
-    app.listen(port, function() {  
-      
-    });  
-    console.log('my api is running on port:' + port);  
+// Variable Port
+//======================================
+const PORT = process.env.PORT || 8080;
+
+//Middleware
+//======================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Routing
+//======================================
+// console.log('routes');
+
+//Lisening to the PORT
+//======================================
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log("Listening on PORT: " + PORT);
+  });
+});
