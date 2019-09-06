2ca81be840193ad1b524b9b9ece94e7fd44973b5
David Schwartz <davidschwartz23@gmail.com>
2019-09-05 17:11:57 -0700

    re added database credentials to config.js

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
-require('dotenv').config();
+equire('dotenv').config();
 module.exports = {
-  development: {
-    username: 'root',
-    password: 'YaNg3918TASAN',
-    database: 'property_manager',
-    host: '127.0.0.1',
-    port: '3306',
-    dialect: 'mysql'
-  },
-  test: {
-    username: 'root',
-    password: 'database_test',
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql'
-  },
-  production: {
-    username: process.env.JAWS_DB_USERNAME,
-    password: process.env.JAWS_DB_PASSWORD,
-    port: process.env.JAWS_DB_PORT,
-    database: process.env.JAWS_DB_DATABASE,
-    dialect: 'mysql'
-  }
-};
+    "development": {
+        "username": "root",
+        "password": "Chicago13",
+        "database": "property_manager",
+        "host": "127.0.0.1",
+        "port": "3306",
+        "dialect": "mysql"
+    },
+    "test": {
+        "username": "root",
+        "password": "database_test",
+        "database": "database_test",
+        "host": "127.0.0.1",
+        "dialect": "mysql"
+    },
+    "production": {
+        "username": process.env.JAWS_DB_USERNAME,
+        "password": process.env.JAWS_DB_PASSWORD,
+        "port": process.env.JAWS_DB_PORT,
+        "database": process.env.JAWS_DB_DATABASE,
+        "dialect": "mysql"
+    }
+}
